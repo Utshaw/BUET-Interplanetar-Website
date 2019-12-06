f2b67f80673a4b51e358c43ea6fe1129a9b84014
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-06 18:40:07 +0600

    Update top bar

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-
-    $('h1').click(function() {
-        $(this).css('background-color', "#ff0000");
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        }else {
+            $('nav').removeClass('sticky');
+        }
+    },
+    {
+        offset: '60px'
     })
+
+
+
+    // var waypoints = $('#options-only').waypoint({
+    //     handler: function (direction) {
+    //         notify(this.element.id + ' hit')
+    //     }
+    // })
+
+
+
+    // $('h1').click(function() {
+    //     $(this).css('background-color', "#ff0000");
+    // })
 });
