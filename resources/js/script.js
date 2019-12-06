b3354799776323543ae65395a2669090f47823cf
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-06 20:06:43 +0600

    Update animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,6 +59,27 @@ $(document).ready(function () {
             }
         });
 
+        // Animation on scroll
+        $('.js--wp-1').waypoint(function(direction) {
+            $('.js--wp-1').addClass('animated fadeIn');
+        },  {
+            offset: '50%'
+        });
 
+
+
+        $('.js--wp-2').waypoint(function(direction) {
+            $('.js--wp-2').addClass('animated pulse');
+        },  {
+            offset: '50%'
+        });
+
+        $('#moments-section').waypoint(function(direction) {
+            $('#moments-section').addClass('animated pulse');
+        },  {
+            offset: '50%'
+        });
+
+        
 
 });
