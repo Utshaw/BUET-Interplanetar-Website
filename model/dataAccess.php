500a0aeaca7c3dabdf2327e71b4db85bc7dc6797
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-08 14:44:05 +0600

    Remove error in dataAccess

---
+++ model/dataAccess.php
@@ -0,0 +1,37 @@
+
+
+<?php
+$connection = new DBConn();
+$pdo = $connection->connect();
+class DAO
+{
+    private static $daoObject;
+    public static function getInstance()
+    {
+        if (!isset(self::$daoObject)) {
+            self::$daoObject =  new DAO();
+        }
+        return self::$daoObject;
+    }
+    private function __construct()
+    { }
+
+
+    
+    public function addCompany($message)
+    {
+        global $pdo;
+        $sql = "INSERT INTO Message(Name, Email, Message, FindSourceID, RecordCreated ) VALUES(:name, :email, :message, :findSourceID, :recordCreated)";
+        $statement = $pdo->prepare($sql);
+
+        $statement->bindValue(':name', $message->NAME, PDO::PARAM_STR);
+        $statement->bindValue(':email', $message->EMAIL, PDO::PARAM_STR);
+        $statement->bindValue(':message', $message->MESSAGE, PDO::PARAM_STR);
+        $statement->bindValue(':findSourceID', $message->FIND_SOURCE_ID, PDO::PARAM_STR);
+        $statement->bindValue(':recordCreated', now(), PDO::PARAM_STR);
+        $statement->execute();
+        $id = $pdo->lastInsertId();
+
+        return $id;
+    }
+}
