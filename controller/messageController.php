70be0f1ba3661647696b3eab1df479aa5d90ac79
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-08 14:47:18 +0600

    Update message controller

--- controller/messageController.php
+++ controller/messageController.php
@@ -8,4 +8,12 @@ $daoObject = DAO::getInstance();
 
 if(isset($_POST['message'])) {
     echo $_POST['name'].  $_POST['email']. $_POST['message'] . $_POST['find-us'];
+
+    $message = new Message();
+    $message->NAME = $_POST['name'];
+    $message->EMAIL = $_POST['email'];
+    $message->MESSAGE = $_POST['message'];
+    $message->FIND_SOURCE_ID = $_POST['find-us'];
+
+    $daoObject->addMessage($message);
 }
