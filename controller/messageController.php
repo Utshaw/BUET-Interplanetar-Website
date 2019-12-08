88c58e424db2b6e1b3685107c62f1fde1df036ba
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-08 14:58:53 +0600

    Updat controller

--- controller/messageController.php
+++ controller/messageController.php
@@ -7,7 +7,6 @@ require_once './model/dataAccess.php';
 $daoObject = DAO::getInstance();
 
 if(isset($_POST['message'])) {
-    echo $_POST['name'].  $_POST['email']. $_POST['message'] . $_POST['find-us'];
 
     $message = new Message();
     $message->NAME = $_POST['name'];
